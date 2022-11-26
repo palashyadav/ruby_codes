20c6db3219742184883c439b2a035c1bfd21072f
Palash Yadav <ypalash67@gmail.com>
2022-11-26 12:00:00 +0530

    Add is_even.js

---
+++ is_even.js
@@ -0,0 +1,10 @@
+// Check if number is even
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(4));          // true
+console.log(isEven(7));          // false
+console.log(isEven(0));          // true
+console.log(isEven(-2));         // true
