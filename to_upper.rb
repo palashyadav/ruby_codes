3eecc9e38d435161768ca2f42f4bc000c6d05a3e
Palash Yadav <ypalash67@gmail.com>
2022-12-05 12:00:00 +0530

    Add to_upper.rb

---
+++ to_upper.rb
@@ -0,0 +1,9 @@
+# Convert string to uppercase
+
+def to_upper(str)
+  str.upcase
+end
+
+puts to_upper("hello")         # HELLO
+puts to_upper("Ruby")          # RUBY
+puts to_upper("test123")       # TEST123
