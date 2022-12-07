d1960bc297b089a7ab80b5cc0946058a85aebcae
Palash Yadav <ypalash67@gmail.com>
2022-12-07 12:00:00 +0530

    Add to_lower.rb

---
+++ to_lower.rb
@@ -0,0 +1,9 @@
+# Convert string to lowercase
+
+def to_lower(str)
+  str.downcase
+end
+
+puts to_lower("HELLO")         # hello
+puts to_lower("Ruby")          # ruby
+puts to_lower("TEST123")       # test123
