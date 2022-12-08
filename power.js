8f2d61076e7db8240ed83330a6bf4555b39b3374
Palash Yadav <ypalash67@gmail.com>
2022-12-08 12:00:00 +0530

    Add power.js

---
+++ power.js
@@ -0,0 +1,10 @@
+// Calculate power of number
+
+function power(base, exp) {
+  return Math.pow(base, exp);
+}
+
+console.log(power(2, 3));      // 8
+console.log(power(5, 2));      // 25
+console.log(power(10, 0));     // 1
+console.log(power(2, 5));      // 32
