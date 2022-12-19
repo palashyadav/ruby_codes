f76d9a0c3394df04755aa071473dc9365eafc2f0
Palash Yadav <ypalash67@gmail.com>
2022-12-19 12:00:00 +0530

    Add abs_value.js

---
+++ abs_value.js
@@ -0,0 +1,10 @@
+// Calculate absolute value
+
+function absValue(num) {
+  return Math.abs(num);
+}
+
+console.log(absValue(5));      // 5
+console.log(absValue(-10));    // 10
+console.log(absValue(0));      // 0
+console.log(absValue(-3.5));   // 3.5
