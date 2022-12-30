9fbac3b3bfe1abd3443f0436aaa861dff4b73ee5
Palash Yadav <ypalash67@gmail.com>
2022-12-30 12:00:00 +0530

    Add is_odd.js

---
+++ is_odd.js
@@ -0,0 +1,10 @@
+// Check if number is odd
+
+function isOdd(num) {
+  return num % 2 !== 0;
+}
+
+console.log(isOdd(5));        // true
+console.log(isOdd(4));        // false
+console.log(isOdd(0));        // false
+console.log(isOdd(-3));       // true
