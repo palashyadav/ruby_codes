e9af53f98ed2c67dc49f63d7f97f00fee7b2df30
Palash Yadav <ypalash67@gmail.com>
2023-01-21 12:00:00 +0530

    Add middle_element.rb

---
+++ middle_element.rb
@@ -0,0 +1,9 @@
+# Get middle element of array
+
+def middle_element(arr)
+  arr[arr.length / 2]
+end
+
+puts middle_element([1, 2, 3, 4, 5])        # 3
+puts middle_element([10, 20, 30])           # 20
+puts middle_element([5])                    # 5
