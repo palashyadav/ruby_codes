4eca0886135ae87dd15119dd5a1b18a2caf38026
Palash Yadav <ypalash67@gmail.com>
2023-01-22 12:00:00 +0530

    Add concatenate.js

---
+++ concatenate.js
@@ -0,0 +1,9 @@
+// Concatenate two strings
+
+function concatenate(str1, str2) {
+  return str1 + str2;
+}
+
+console.log(concatenate("Hello", " World"));    // Hello World
+console.log(concatenate("Ruby", "JS"));          // RubyJS
+console.log(concatenate("", "test"));            // test
