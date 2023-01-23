62fb9011f270a5758783293cf4ef1f4affbde843
Palash Yadav <ypalash67@gmail.com>
2023-01-23 12:00:00 +0530

    Add repeat_string.rb

---
+++ repeat_string.rb
@@ -0,0 +1,9 @@
+# Repeat string n times
+
+def repeat_string(str, n)
+  str * n
+end
+
+puts repeat_string("ab", 3)       # ababab
+puts repeat_string("x", 5)        # xxxxx
+puts repeat_string("hi", 1)       # hi
