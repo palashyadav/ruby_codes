5c120d300df2b32249270a7b7ab073156473d3a2
Palash Yadav <ypalash67@gmail.com>
2023-02-01 12:00:00 +0530

    Add round_number.rb

---
+++ round_number.rb
@@ -0,0 +1,10 @@
+# Round number to nearest integer
+
+def round_number(num)
+  num.round
+end
+
+puts round_number(3.14)        # 3
+puts round_number(3.5)         # 4
+puts round_number(3.9)         # 4
+puts round_number(2.1)         # 2
