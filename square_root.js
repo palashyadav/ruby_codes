cb0ffcd6578fe8da3aa55c2551a297326caeb6b8
Palash Yadav <ypalash67@gmail.com>
2023-02-07 12:00:00 +0530

    Add square_root.js

---
+++ square_root.js
@@ -0,0 +1,10 @@
+// Square root of number
+
+function squareRoot(num) {
+  return Math.sqrt(num);
+}
+
+console.log(squareRoot(16));       // 4
+console.log(squareRoot(25));       // 5
+console.log(squareRoot(2));        // 1.414...
+console.log(squareRoot(0));        // 0
