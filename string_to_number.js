8f1d176bdf40ca025d98df7a5d27975284ad8f21
Palash Yadav <ypalash67@gmail.com>
2023-02-12 12:00:00 +0530

    Add string_to_number.js

---
+++ string_to_number.js
@@ -0,0 +1,9 @@
+// Convert string to number
+
+function stringToNumber(str) {
+  return Number(str);
+}
+
+console.log(stringToNumber("123"));         // 123
+console.log(stringToNumber("45.67"));       // 45.67
+console.log(stringToNumber("abc"));         // NaN
