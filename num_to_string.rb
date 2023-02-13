c18f3b4585ca8280ffd0c0cc182ea4eac91baf8f
Palash Yadav <ypalash67@gmail.com>
2023-02-13 12:00:00 +0530

    Add num_to_string.rb

---
+++ num_to_string.rb
@@ -0,0 +1,9 @@
+# Convert number to string
+
+def num_to_string(num)
+  num.to_s
+end
+
+puts num_to_string(123)        # "123"
+puts num_to_string(45.67)      # "45.67"
+puts num_to_string(0)          # "0"
