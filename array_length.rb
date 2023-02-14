f02d6b2a319217d1c67114effcb9c6620e89c140
Palash Yadav <ypalash67@gmail.com>
2023-02-14 12:00:00 +0530

    Add array_length.rb

---
+++ array_length.rb
@@ -0,0 +1,9 @@
+# Get array length
+
+def array_length(arr)
+  arr.length
+end
+
+puts array_length([1, 2, 3, 4, 5])     # 5
+puts array_length([])                  # 0
+puts array_length(["a", "b"])          # 2
