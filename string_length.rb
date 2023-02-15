4019b195b40e36c5f3c9b6566b721acbba81ff03
Palash Yadav <ypalash67@gmail.com>
2023-02-15 12:00:00 +0530

    Add string_length.rb

---
+++ string_length.rb
@@ -0,0 +1,9 @@
+# Get string length
+
+def string_length(str)
+  str.length
+end
+
+puts string_length("hello")        # 5
+puts string_length("")             # 0
+puts string_length("test")         # 4
