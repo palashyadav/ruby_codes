8a71d73f97343a0fefd82d0a38e68829b4a0cf71
Palash Yadav <ypalash67@gmail.com>
2023-03-03 12:00:00 +0530

    Add coin_change.rb

---
+++ coin_change.rb
@@ -0,0 +1,19 @@
+# Coin Change: Minimum coins to make amount
+
+def coin_change(coins, amount)
+  dp = Array.new(amount + 1, Float::INFINITY)
+  dp[0] = 0
+  
+  coins.each do |coin|
+    (coin..amount).each do |i|
+      dp[i] = [dp[i], dp[i - coin] + 1].min
+    end
+  end
+  
+  dp[amount] == Float::INFINITY ? -1 : dp[amount]
+end
+
+puts coin_change([1, 2, 5], 5)
+puts coin_change([2], 3)
+puts coin_change([10], 1)
+puts coin_change([1, 3, 4], 6)
