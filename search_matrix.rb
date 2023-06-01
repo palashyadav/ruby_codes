b25b305f1c02728e81a7c3969dc64f776cdcfa1c
Palash Yadav <ypalash67@gmail.com>
2023-06-01 12:00:00 +0530

    Add search_matrix.rb (June 1): Search a 2D Matrix

---
+++ search_matrix.rb
@@ -0,0 +1,21 @@
+# Search a 2D Matrix: Search for a target value in a 2D matrix with sorted rows and columns
+def search_matrix(matrix, target)
+  return false if matrix.empty? || matrix[0].empty?
+  m, n = matrix.size, matrix[0].size
+  left, right = 0, m * n - 1
+  while left <= right
+    mid = (left + right) / 2
+    val = matrix[mid / n][mid % n]
+    if val == target
+      return true
+    elsif val < target
+      left = mid + 1
+    else
+      right = mid - 1
+    end
+  end
+  false
+end
+
+puts search_matrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3)
+puts search_matrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13)
