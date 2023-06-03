0c9564c7b455923401d30f0d9be0101d529c49b5
Palash Yadav <ypalash67@gmail.com>
2023-06-03 12:00:00 +0530

    Add find_min_rotated_array.js (June 3): Find Minimum in Rotated Array (JS)

---
+++ find_min_rotated_array.js
@@ -0,0 +1,16 @@
+// Find Minimum in Rotated Sorted Array: Return the minimum element
+function findMin(nums) {
+    let left = 0, right = nums.length - 1;
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+    return nums[left];
+}
+
+console.log(findMin([3,4,5,1,2]));
+console.log(findMin([4,5,6,7,0,1,2]));
