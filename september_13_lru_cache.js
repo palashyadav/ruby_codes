db36a99c8829445414c108b67914250ca28788d4
Palash Yadav <ypalash67@gmail.com>
2023-09-13 09:00:00 +0530

    Add september_13_lru_cache.js (Sep 13): LRU Cache (JS, randomized)

---
+++ september_13_lru_cache.js
@@ -0,0 +1,29 @@
+// LRU Cache (JS): simple LRU with Map and ordering
+class LRUCache {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.cache = new Map();
+  }
+  get(key) {
+    if (!this.cache.has(key)) return -1;
+    const val = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, val);
+    return val;
+  }
+  put(key, value) {
+    if (this.cache.has(key)) this.cache.delete(key);
+    this.cache.set(key, value);
+    if (this.cache.size > this.capacity) {
+      const firstKey = this.cache.keys().next().value;
+      this.cache.delete(firstKey);
+    }
+  }
+}
+
+const c = new LRUCache(2);
+c.put(1,1);
+c.put(2,2);
+console.log(c.get(1));
+c.put(3,3);
+console.log(c.get(2));
